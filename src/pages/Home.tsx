2e3bdc585896b55168d326d349170dff11f58819
Argayash <zilberman.yakoff@yandex.ru>
2024-07-23 00:14:01 +0300

    fix: оставил только Header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Footer, Header, Main } from '../components'
+import React from 'react';
+import { Footer, Header, Main } from '../components';
 
 export const Home = () => {
-	return (
-		<div>
-			<Header />
-			<Main />
-			<Footer />
-		</div>
-	)
-}
+  return (
+    <div>
+      <Header />
+      {/* <Main />
+			<Footer /> */}
+    </div>
+  );
+};
